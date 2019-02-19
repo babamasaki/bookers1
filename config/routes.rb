b08e1cbee6ad03b57fa5ac87efb1150e622f1019
babamasaki <0508baba@gmail.com>
2019-02-19 04:33:12 +0000

    再提出

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
 
+ get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
+
   root to:'books#top'
 end
